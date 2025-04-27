fb0029cd02a92a8537cf77a890ba8f5986dd47f2
xiaoqiang.yao <yaoxiaoqiang@gnlab.com>
2025-04-27 15:49:18 +0800

    更新package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  build: {
+    emptyOutDir: true,
+    lib: {
+      entry: "./src/components/index.ts",
+      name: "variable-editor",
+      fileName: (format) => `variable-editor.${format}.js`,
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: { vue: "Vue" },
+      },
+    },
+  },
+});
